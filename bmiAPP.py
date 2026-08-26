1847e9f9d3ae35fc0fc661be6118399a0a5c75ad
402-01-Pimchanok <54122@yupparaj.ac.th>
2026-08-26 15:31:54 +0700

    Create bmiAPP.py

---
+++ bmiAPP.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.markdown("# :red[💪 คำนวณค่าดัชนีมวลกาย]")
+st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
+
+weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+
+if st.buttom("คำนวณค่า BMI 🎯"):
+  height_m = height_cm / 100
+  bmi = weight / (height_m ** 2)
+
+ st.write("---")
+ st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+
+if 18.5 <= bmi < 23.0:
+  st.warning("❌ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+else bmi < 18.5:
+  st.success("🎉 คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+else 23.0 <= bmi < 25.0:
+  st.info("⚠️ คุณเริ่มมีน้ำหนักเกินกว่าเกณฑ์ (ท้วม)")
+else:
+  st.error("🚨 คุณมีน้ำหนักอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและควรออกกำลังกาย")
+
+st.divider()
+st.write("นางสาวพิมพ์ชนก ซัง เลขที่ 1 ม.4/2")
